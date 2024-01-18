8063204a62ddef9a366e5e5f5f2ad919c1ba46ad
admin <admin@DESKTOP-U7BSKO7>
2024-01-18 09:02:15 +0900

    ㅊ3

--- hgjava/src/com/yedam/todo/Todo0117.java
+++ hgjava/src/com/yedam/todo/Todo0117.java
@@ -1,10 +1,21 @@
 package com.yedam.todo;
 
+import java.util.Scanner;
+
 public class Todo0117 {
 	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
 		// 친구3명의 이름과 연락처를 입력하도록..메세지출력.
 		// 입력완료.
-
+		String str = "";
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("이름>>>");
+			str += "이름: " + scn.nextLine() + "\n";
+			System.out.println("연락처>>>");
+			str += "연락처: " + scn.nextLine() + "\n";
+			str += "========================\n";
+		}
+		System.out.println(str);
 		// 이름: 홍길동
 		// 연락처: 010-1111-2222
 		// ====================
